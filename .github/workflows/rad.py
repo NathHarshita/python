37d6688266d2f8aa45015aaaf69d6738a0a1a922
NathHarshita <66841242+NathHarshita@users.noreply.github.com>
2020-06-16 22:22:11 +0530

    rad.py
    program to accept the radius of a circle from users anf computes area?

---
+++ .github/workflows/rad.py
@@ -0,0 +1,5 @@
+#Programm to accept the radius of a circle from users and computes area!
+import math
+radius=eval(input("Enter radius:"))
+Area= (3.14)*(radius)*(radius)
+print("Area of circle",Area)
